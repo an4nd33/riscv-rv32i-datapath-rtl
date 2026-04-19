3f060ca3867e0e843a1657ecad7eeebdc706c6c4
Anand P <148001932+an4nd33@users.noreply.github.com>
2026-04-19 09:18:35 +0530

    Create gls.f with library and design file paths
    Added paths for SAED14 HVT, LVT, and RVT libraries along with design files.

---
+++ gls/gls.f
@@ -0,0 +1,36 @@
+# ===============================
+# SAED14 HVT
+# ===============================
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_HVT/verilog/base/*.v
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_HVT/verilog/cg/*.v
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_HVT/verilog/dlv/*.v
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_HVT/verilog/iso/*.v
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_HVT/verilog/pg/*.v
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_HVT/verilog/ret/*.v
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_HVT/verilog/ulvl/*.v
+
+# ===============================
+# SAED14 LVT
+# ===============================
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_LVT/verilog/base/*.v
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_LVT/verilog/cg/*.v
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_LVT/verilog/dlv/*.v
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_LVT/verilog/iso/*.v
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_LVT/verilog/pg/*.v
+
+# ===============================
+# SAED14 RVT
+# ===============================
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_RVT/verilog/base/*.v
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_RVT/verilog/cg/*.v
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_RVT/verilog/dlv/*.v
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_RVT/verilog/iso/*.v
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_RVT/verilog/pg/*.v
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_RVT/verilog/ret/*.v
+/home/synopsys/installs/LIBRARIES/SAED14_EDK/SAED14nm_EDK_STD_RVT/verilog/ulvl/*.v
+
+# ===============================
+# YOUR DESIGN (IMPORTANT)
+# ===============================
+./top_system_10ns_netlist.v
+./tb_soc.v
